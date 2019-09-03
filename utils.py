221eac64720a9c968709ccb7f26a390708d0c5cb
Luciano Hoult <futurehult@hotmail.com>
2019-09-03 16:54:56 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Author: Brandon Han
-# @Date:   2019-08-17 15:20:26
-# @Last Modified by:   BrandonHanx
-# @Last Modified time: 2019-08-20 18:16:35
+
 import torch
 import os
 import json
@@ -11,8 +8,11 @@ import cmath
 import numpy as np
 from scipy import interpolate
 import scipy.io as scio
+import matlab.engine
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
+Tensor = torch.cuda.DoubleTensor if torch.cuda.is_available() else torch.DoubleTensor
+
 
 class Params():
     """Class that loads hyperparameters from a json file.
@@ -94,11 +94,12 @@ def plot_single_part(wavelength, spectrum, name, legend='spectrum', interpolate=
     plt.title(legend)
     plt.xlabel('Wavelength (nm)')
     plt.ylabel(legend)
+    plt.ylim((0, 1))
     plt.savefig(save_dir)
     plt.close()
 
 
-def plot_both_parts(wavelength, real, fake, name, interpolate=True):
+def plot_both_parts(wavelength, real, fake, name, legend='Real and Fake', interpolate=True):
 
     color_left = 'blue'
     color_right = 'red'
@@ -129,7 +130,7 @@ def plot_both_parts(wavelength, real, fake, name, interpolate=True):
     ax2.spines['left'].set_color(color_left)
     ax2.spines['right'].set_color(color_right)
     plt.ylim((0, 1))
-    plt.title('Real and Fake')
+    plt.title(legend)
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.savefig(save_dir)
@@ -161,11 +162,11 @@ def polar2rect_parallel(modu_que, phase_que):
     return real_que, imag_que
 
 
-def find_spectrum(thickness, radius, TT_array):
+def find_spectrum(thickness, radius, gap, TT_array):
     rows, _ = TT_array.shape
     wavelength, spectrum = [], []
     for row in range(rows):
-        if TT_array[row, 1] == thickness and TT_array[row, 2] == radius:
+        if TT_array[row, 1] == thickness and TT_array[row, 2] == radius and TT_array[row, 3] == gap:
             wavelength.append(TT_array[row, 0])
             spectrum.append(TT_array[row, -1])
         else:
@@ -186,6 +187,143 @@ def load_mat(path):
 
 
 def data_pre(list_all, wlimit):
+    dtype = [('wave_length', int), ('thickness', int), ('radius', int), ('gap', int), ('efficiency', float)]
+    values = [tuple(single_device) for single_device in list_all]
+    array_temp = np.array(values, dtype)
+    array_all = np.sort(array_temp, order=['thickness', 'radius', 'gap', 'wave_length'])
+
+    thickness_list = np.unique(array_all['thickness'])
+    radius_list = np.unique(array_all['radius'])
+    gap_list = np.unique(array_all['gap'])
+    reformed = []
+
+    for thickness in thickness_list:
+        for radius in radius_list:
+            for gap in gap_list:
+                pick_index = np.intersect1d(np.argwhere(array_all['radius'] == radius), np.argwhere(
+                    array_all['thickness'] == thickness), np.argwhere(array_all['gap'] == gap))
+                picked = array_all[pick_index]
+                picked = np.sort(picked, order=['wave_length'])
+                cur_ref = [thickness, radius, gap]
+                for picked_single in picked:
+                    cur_ref.append(picked_single[4])
+
+                reformed.append(cur_ref)
+
+    return np.array(reformed), array_all
+
+
+def inter(inputs, device):
+    inputs_inter = torch.ones(inputs.shape[0], inputs.shape[1], 224)
+    x = np.linspace(0, 223, num=inputs.shape[2])
+    new_x = np.linspace(0, 223, num=224)
+
+    for index_j, j in enumerate(inputs):
+        for index_jj, jj in enumerate(j):
+            y = jj
+            f = interpolate.interp1d(x, y, kind='cubic')
+            jj = f(new_x)
+            inputs_inter[index_j, index_jj, :] = torch.from_numpy(jj)
+
+    inputs_inter = inputs_inter.double().to(device)
+
+    return inputs_inter
+
+
+def RCWA_parallel(eng, w_list, thick_list, r_list, gap, acc=5):
+    batch_size = len(thick_list)
+    spec = np.ones((batch_size, len(w_list)))
+    gap = matlab.double([gap])
+    acc = matlab.double([acc])
+    for i in range(batch_size):
+        thick = thick_list[i]
+        thick = matlab.double([thick])
+        r = r_list[i]
+        r = matlab.double([r])
+        for index, w in enumerate(w_list):
+            w = matlab.double([w])
+            spec[i, index] = eng.RCWA_solver(w, gap, thick, r, acc)
+
+    return spec
+
+
+def RCWA(eng, w_list, thick, r, gap, acc=5, medium=1, shape=0):
+    spec = np.ones(len(w_list))
+    gap = matlab.double([gap])
+    acc = matlab.double([acc])
+    thick = matlab.double([thick])
+    r = matlab.double([r])
+    medium = matlab.double([medium])
+    shape = matlab.double([shape])
+    for index, w in enumerate(w_list):
+        w = matlab.double([w])
+        spec[index] = eng.RCWA_solver(w, gap, thick, r, acc, medium, shape)
+
+    return spec
+
+
+def RCWA_call(eng, w_list, g_list, t_list, r_list, acc=5):
+    batch_size = len(t_list)
+    spec = np.ones((batch_size, len(w_list)))
+    for i in range(batch_size):
+        thickness = t_list[i]
+        thickness = matlab.double([thickness])
+        gap = g_list[i]
+        gap = matlab.double([gap])
+        radius = r_list[i]
+        radius = matlab.double([radius])
+        acc = matlab.double([acc])
+        wavelength = matlab.double(w_list)
+        result = eng.RCWA_solver_par(wavelength, gap, thickness, radius, acc)
+        result2 = np.asarray(result).tolist()
+        spec[i] = result2[0]
+
+    return spec
+
+
+def normalization(data):
+    _range = np.max(data) - np.min(data)
+    return (data - np.min(data)) / _range
+
+
+def keep_range(data):
+    for i in range(len(data)):
+        if data[i] < 0:
+            data[i] = 0
+        elif data[i] > 1:
+            data[i] = 1
+        else:
+            continue
+    return data
+
+
+def gauss_spec_valley(f, mean, var, depth=0.2):
+    return 1 - (1 - depth) * np.exp(-(f - mean) ** 2 / (2 * (var ** 2)))
+
+
+def gauss_spec_peak(f, mean, var, depth=0.2):
+    return (1 - depth) * np.exp(-(f - mean) ** 2 / (2 * (var ** 2)))
+
+
+def random_gauss_spec(f):
+    depth = np.random.uniform(low=0.0, high=0.05)
+    mean = np.random.uniform(low=460, high=490)
+    var = np.random.uniform(low=20, high=40)
+    return 1 - (1 - depth) * np.exp(-(f - mean) ** 2 / (2 * (var ** 2)))
+
+
+def random_gauss_spec_combo(f, valley_num):
+    spec = np.zeros(len(f))
+    for i in range(valley_num):
+        spec += random_gauss_spec(f)
+    return normalization(spec)
+
+
+def spec_jitter(spec, amp):
+    return normalization(spec + np.random.uniform(low=-amp, high=amp, size=spec.size))
+
+
+def unique_device(list_all):
     dtype = [('wave_length', int), ('thickness', int), ('radius', int), ('efficiency', float)]
     values = [tuple(single_device) for single_device in list_all]
     array_temp = np.array(values, dtype)
@@ -193,6 +331,7 @@ def data_pre(list_all, wlimit):
     
     thickness_list = np.unique(array_all['thickness'])
     radius_list = np.unique(array_all['radius'])
+
     reformed = []
 
     for thickness in thickness_list:
@@ -200,24 +339,17 @@ def data_pre(list_all, wlimit):
             pick_index = np.intersect1d(np.argwhere(array_all['radius'] == radius), np.argwhere(array_all['thickness'] == thickness))
             if pick_index.size == 0:
                 continue
-            picked = array_all[pick_index]
-            picked = np.sort(picked, order = ['wave_length'])
-            cur_ref = [thickness, radius]
-            for picked_single in picked:
-                cur_ref.append(picked_single[3])
-            
-            # if len(cur_ref) > wlimit + 2:
-            #     cur_ref = cur_ref[0:wlimit + 2]
+            pair = [thickness, radius]
 
-            reformed.append(cur_ref)
+            reformed.append(pair)
 
-    return np.array(reformed), array_all
+    return reformed
 
 
 if __name__ == "__main__":
 
     data_path = current_dir + '\\data'
-    save_path = current_dir + '\\sorted'
+    save_path = current_dir + '\\device_id'
     files = os.listdir(data_path)
 
     data_list_all = []
@@ -228,13 +360,16 @@ if __name__ == "__main__":
         data_list, _ = load_mat(path)
         data_list_all.extend(data_list)
 
-    data_prepared, data_sorted = data_pre(data_list_all, None)
-    scio.savemat(save_path, {'value': data_prepared})
+    # data_prepared, data_sorted = data_pre(data_list_all, None)
+    # scio.savemat(save_path, {'value': data_prepared})
 
     # pick_index = np.intersect1d(np.argwhere(data_sorted['radius'] == 20), np.argwhere(data_sorted['thickness'] == 200))
 
     # picked_data = aray[pick_index]
 
     # sliced = array_slice(data_sorted)
+
+    device_id = unique_device(data_list_all)
+    scio.savemat(save_path, {'device_id': device_id})
     
     print('done')
